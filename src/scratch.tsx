f7d7d272a7222412580b0408624aa6ca708079a8
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-18 16:42:37 +0200

    deleted scratch

--- src/scratch.tsx
+++
@@ -1,5 +0,0 @@
-onError: (err, id, context) => {
-    if (context?.previousBoodschappen) {
-        queryClient.setQueryData<Boodschap[]>(CACHE_KEY_BOODSCHAPPEN, context.previousBoodschappen);
-    }
-},
