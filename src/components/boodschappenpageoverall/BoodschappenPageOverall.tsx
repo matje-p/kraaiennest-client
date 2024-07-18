1ac0c6ff243f162b20b5f998d722ba9c02cb1923
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-18 19:02:56 +0200

    port test

--- src/components/boodschappenpageoverall/BoodschappenPageOverall.tsx
+++ src/components/boodschappenpageoverall/BoodschappenPageOverall.tsx
@@ -2,6 +2,7 @@ import BoodschappenPage from "../boodschappenpage/BoodschappenPage";
 
 const BoodschappenPageOverall = () => {
   console.log("Rendering BoodschappenPageOverall");
+  console.log("VITE_API_URL", import.meta.env.VITE_API_URL);
   return <BoodschappenPage />;
 };
 
