595c7605fcf353667c383390641cc697bd0d4a95
matje-p <138682310+matje-p@users.noreply.github.com>
2024-07-23 15:20:33 +0200

    added more spinners

--- src/components/boodschappenpage/BoodschappenPage.tsx
+++ src/components/boodschappenpage/BoodschappenPage.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import useBoodschappen from "../../hooks/useBoodschappen";
 import BoodschapRow from "../boodschaprow/BoodschapRow";
 import Header from "../header/Header";
+import Spinner from "../spinner/Spinner";
 
 const BoodschappenPage: React.FC = () => {
   console.log("BoodschappenPage rendered");
   const { data: boodschappen, error, isLoading } = useBoodschappen();
-  if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <Spinner />;
   if (error) return <p>{error.message}</p>;
 
   return (
