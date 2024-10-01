16e89f807be5e3f60bccced7a3a9a1700817b7f3
matje-p <138682310+matje-p@users.noreply.github.com>
2024-10-01 21:09:23 +0200

    trigger build

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useUser } from "./auth/userContext";
 import { Outlet } from "react-router-dom";
-// import BoodschappenPage from "./components/boodschappenpage/BoodschappenPage";
 import Spinner from "./components/spinner/Spinner";
 import useHouseholds from "./components/boodschappenpage/header/householdselector/useHouseholds";
 import useHouseholdStore from "./components/boodschappenpage/header/householdselector/householdStore";
