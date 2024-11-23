6b577f06bc1cf9434bd0fbb2c9a920a92937e383
matje-p <138682310+matje-p@users.noreply.github.com>
2024-11-23 17:17:34 +0100

    more updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// Import global styles first (which includes Bootstrap SCSS)
 import "./global.scss";
+// Then Bootstrap icons
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// Remove this line since Bootstrap is already imported in global.scss
+// import "bootstrap/dist/css/bootstrap.min.css";
+// Keep Bootstrap JS
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import AuthProvider from "./auth/AuthProvider.tsx";
-import styles from "./index.module.scss"; // Corrected import
+import styles from "./index.module.scss";
 import router from "./routing/routes.tsx";
 
 const queryClient = new QueryClient();
