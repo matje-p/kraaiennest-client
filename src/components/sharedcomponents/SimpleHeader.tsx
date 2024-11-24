22a329d79cecf89baa2c1d683ea12472516ce13d
matje-p <138682310+matje-p@users.noreply.github.com>
2024-11-24 21:50:58 +0100

    simplified simple header

--- src/components/sharedcomponents/SimpleHeader.tsx
+++ src/components/sharedcomponents/SimpleHeader.tsx
@@ -1,19 +1,32 @@
-import BackButton from "./BackButton";
+import { Link, useNavigate } from "react-router-dom";
 import SettingsButton from "./SettingsButton";
-import HomeButton from "./HomeButton";
+import styles from "./SimpleHeader.module.scss";
 
 interface SimpleHeaderProps {
   settings?: boolean;
 }
 
 const SimpleHeader = ({ settings = false }: SimpleHeaderProps) => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1); // Goes back one step in history
+  };
+
   return (
     <div>
       <div className="d-flex justify-content-between">
-        <BackButton />
+        <button
+          onClick={handleBack}
+          className={`btn btn-primary ${styles.Button}`}
+        >
+          <i className="bi bi-arrow-left fs-3"></i>
+        </button>
         <div>
           {settings && <SettingsButton />}
-          <HomeButton />
+          <Link to="/" className={`btn btn-primary ${styles.Button}`}>
+            <i className="bi bi-house fs-3"></i>
+          </Link>
         </div>
       </div>
     </div>
